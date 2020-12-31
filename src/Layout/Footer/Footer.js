2882f2adc575964f5f1c3514d5ceebced3d82a5e
Anirudh <anirudhv@purdue.edu>
2020-12-31 05:27:06 -0800

    Updated social media icon redirection

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -5,8 +5,8 @@ const Footer = (props) => (
 	<div className={modules.Footer}>
 	<a href="https://twitter.com/anirudhvenky" class={modules.SocialMedia}><i class="fa fa-twitter"></i></a>
     <a href="mailto:anirudhv@purdue.edu" class={modules.SocialMedia}><i class="fa fa-envelope"></i></a>
-  	<a href="https://www.linkedin.com/in/anirudh-venkataramanan-2811651a5/" class={modules.SocialMedia}><i class="fa fa-github"></i></a>
-  	<a href="https://github.com/anirudhv" class={modules.SocialMedia}><i class="fa fa-linkedin"></i></a>
+  	<a href="https://www.linkedin.com/in/anirudh-venkataramanan-2811651a5/" class={modules.SocialMedia}><i class="fa fa-linkedin"></i></a>
+  	<a href="https://github.com/anirudhv" class={modules.SocialMedia}><i class="fa fa-github"></i></a>
 	</div>
 );
 
