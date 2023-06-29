079be780fac5c72d89e3ea06665f89cff59fe2cd
Anirudh Venkataramanan <anirudh@ucsc.edu>
2023-06-28 20:53:01 -0700

    Responsive changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	distDir: 'build'
-}
+  reactStrictMode: true,
+  swcMinify: true,
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
